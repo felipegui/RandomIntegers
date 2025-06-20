4508e60d830c32ed8fbcb11584ca6ed4e6875850
Felipe Guimaraes <fguiengenharia@gmail.com>
2025-06-20 14:10:10 -0300

    Simulando jogar um dado de 6 faces seis mil vezes e verifcar a frequencia de vezes que cada lado cai

--- RandomIntegers/src/App.java
+++ RandomIntegers/src/App.java
@@ -1,21 +1,43 @@
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        int value;
-        String output = "";
 
-        for (int counter = 1; counter <= 20; counter++) {
-            value = 1 + (int) (Math.random() * 6);
+        int frequency1 = 0, frequency2 = 0, frequency3 = 0,
+                frequency4 = 0, frequency5 = 0, frequency6 = 0, face;
 
-            output += value + " ";
+        for (int roll = 1; roll <= 6000; roll++) {
+            face = 1 + (int) (Math.random() * 6);
 
-            if (counter % 5 == 0) {
-                output += "\n";
+            switch (face) {
+                case 1:
+                    ++frequency1;
+                    break;
+                case 2:
+                    ++frequency2;
+                    break;
+                case 3:
+                    ++frequency3;
+                    break;
+                case 4:
+                    ++frequency4;
+                    break;
+                case 5:
+                    ++frequency5;
+                    break;
+                case 6:
+                    ++frequency6;
+                    break;
             }
         }
-        JOptionPane.showMessageDialog(null, output, "20 Random Numbers from 1 to 6 ", JOptionPane.INFORMATION_MESSAGE);
+
+        JTextArea outputArea = new JTextArea();
+
+        outputArea.setText("Face\tFrequency" + "\n1\t" + frequency1 + "\n2\t" + frequency2 + "\n3\t" + frequency3
+                + "\n4\t" + frequency4 + "\n5\t" + frequency5 + "\n6\t" + frequency6);
+        
+        JOptionPane.showMessageDialog(null, outputArea, "Rolling a Dice 6000 Times", JOptionPane.INFORMATION_MESSAGE);
 
         System.exit(0);
     }
