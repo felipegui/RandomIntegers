9531f9b415c9b28aaaa2a340ef725ec0d653c2fb
Felipe Guimaraes <fguiengenharia@gmail.com>
2025-06-20 13:41:47 -0300

    Numero inteiro aleatorios deslocados

---
+++ RandomIntegers/src/App.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int value;
+        String output = "";
+
+        for (int counter = 1; counter <= 20; counter++) {
+            value = 1 + (int) (Math.random() * 6);
+
+            output += value + " ";
+
+            if (counter % 5 == 0) {
+                output += "\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null, output, "20 Random Numbers from 1 to 6 ", JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+    }
+}
